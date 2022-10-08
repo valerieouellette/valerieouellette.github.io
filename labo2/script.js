5108a108740a9c198b47f482faf66b75abc1834e
valerieo <valerieouellette@icloud.com>
2022-10-08 17:31:31 -0400

    Update script.js

--- labo2/script.js
+++ labo2/script.js
@@ -2,3 +2,4 @@ var asCa = document.createElement('img');
 asCa.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/02_of_diamonds.svg/langfr-300px-02_of_diamonds.svg.png"
 var carte11 = document.getElementById('carte1.1')
 carte11.appendChild(asCa)
+
