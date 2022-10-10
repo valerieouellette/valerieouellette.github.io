c474991967373244c18afb324135c6489b7c3129
valerieo <valerieouellette@icloud.com>
2022-10-09 21:29:48 -0400

    création éléments img

--- labo2/script.js
+++ labo2/script.js
@@ -1,5 +1,116 @@
-var asCa = document.createElement('img');
-asCa.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/02_of_diamonds.svg/langfr-300px-02_of_diamonds.svg.png"
-var carte11 = document.getElementById('carte1.1')
-carte11.appendChild(asCa)
+var carreauAs = document.createElement('img');
+carreauAs.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/02_of_diamonds.svg/langfr-300px-02_of_diamonds.svg.png"
+var piqueAs = document.createElement('img');
+piqueAs.src = ""
+var coeurAs = document.createElement('img');
+coeurAs.src = ""
+var trefleAs = document.createElement('img');
+trefleAs.src = ""
 
+var carreau2 = document.createElement('img');
+carreau2.src = ""
+var pique2 = document.createElement('img');
+pique2.src = ""
+var coeur2 = document.createElement('img');
+coeur2.src = ""
+var trefle2 = document.createElement('img');
+trefle2.src = ""
+
+var carreau3 = document.createElement('img');
+carreau3.src = ""
+var pique3 = document.createElement('img');
+pique3.src = ""
+var coeur3 = document.createElement('img');
+coeur3.src = ""
+var trefle3 = document.createElement('img');
+trefle3.src = ""
+
+var carreau4 = document.createElement('img');
+carreau4.src = ""
+var pique4 = document.createElement('img');
+pique4.src = ""
+var coeur4 = document.createElement('img');
+coeur4.src = ""
+var trefle4 = document.createElement('img');
+trefle4.src = ""
+
+var carreau5 = document.createElement('img');
+carreau5.src = ""
+var pique5 = document.createElement('img');
+pique5.src = ""
+var coeur5 = document.createElement('img');
+coeur5.src = ""
+var trefle5 = document.createElement('img');
+trefle5.src = ""
+
+var carreau6 = document.createElement('img');
+carreau6.src = ""
+var pique6 = document.createElement('img');
+pique6.src = ""
+var coeur6 = document.createElement('img');
+coeur6.src = ""
+var trefle6 = document.createElement('img');
+trefle6.src = ""
+
+var carreau7 = document.createElement('img');
+carreau7.src = ""
+var pique7 = document.createElement('img');
+pique7.src = ""
+var coeur7 = document.createElement('img');
+coeur7.src = ""
+var trefle7 = document.createElement('img');
+trefle7.src = ""
+
+var carreau8 = document.createElement('img');
+carreau8.src = ""
+var pique8 = document.createElement('img');
+pique8.src = ""
+var coeur8 = document.createElement('img');
+coeur8.src = ""
+var trefle8 = document.createElement('img');
+trefle8.src = ""
+
+var carreau9 = document.createElement('img');
+carreau9.src = ""
+var pique9 = document.createElement('img');
+pique9.src = ""
+var coeur9 = document.createElement('img');
+coeur9.src = ""
+var trefle9 = document.createElement('img');
+trefle9.src = ""
+
+var carreau10 = document.createElement('img');
+carreau10.src = ""
+var pique10 = document.createElement('img');
+pique10.src = ""
+var coeur10 = document.createElement('img');
+coeur10.src = ""
+var trefle10 = document.createElement('img');
+trefle10.src = ""
+
+var carreauJ = document.createElement('img');
+carreauJ.src = ""
+var piqueJ = document.createElement('img');
+piqueJ.src = ""
+var coeurJ = document.createElement('img');
+coeurJ.src = ""
+var trefleJ = document.createElement('img');
+trefleJ.src = ""
+
+var carreauQ = document.createElement('img');
+carreauQ.src = ""
+var piqueQ = document.createElement('img');
+piqueQ.src = ""
+var coeurQ = document.createElement('img');
+coeurQ.src = ""
+var trefleQ = document.createElement('img');
+trefleQ.src = ""
+
+var carreauK = document.createElement('img');
+carreauK.src = ""
+var piqueK = document.createElement('img');
+piqueK.src = ""
+var coeurK = document.createElement('img');
+coeurK.src = ""
+var trefleK = document.createElement('img');
+trefleK.src = ""
